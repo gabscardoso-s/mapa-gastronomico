ff8baeabacc6d6670198e598bf972a8600760206
Gabriel Cardoso <g.c.souza91@gmail.com>
2025-04-19 21:56:49 -0300

    chore(routing): configure app-routing
    Configure routes in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,42 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'add-place',
+    loadChildren: () =>
+      import('./pages/add-place/add-place.module').then(
+        (m) => m.AddPlacePageModule
+      ),
+  },
+  {
+    path: 'places-list',
+    loadChildren: () =>
+      import('./pages/places-list/places-list.module').then(
+        (m) => m.PlacesListPageModule
+      ),
+  },
+  {
+    path: 'add-place',
+    loadChildren: () => import('./pages/add-place/add-place.module').then( m => m.AddPlacePageModule)
+  },
+  {
+    path: 'places-list',
+    loadChildren: () => import('./pages/places-list/places-list.module').then( m => m.PlacesListPageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
