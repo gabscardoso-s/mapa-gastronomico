ebffa7a060ef8be434fe6c293f1fed6631f33452
Gabriel Cardoso <g.c.souza91@gmail.com>
2025-05-04 15:51:02 -0300

    chore(android): add Capacitor Android platform
    Initialize Android platform using Capacitor and configure native project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app-pokeradar',
-  webDir: 'www'
+  appName: 'mapa-gastronomico',
+  webDir: 'www',
 };
 
 export default config;
